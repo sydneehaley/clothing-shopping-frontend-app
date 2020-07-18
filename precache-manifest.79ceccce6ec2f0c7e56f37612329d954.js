d925d28f5170e1214635b54d48f3020b01ee18f9
Sydnee Haley <hello@sydneehaley.com>
2020-07-17 20:21:29 -0400

    Updates

---
+++ precache-manifest.79ceccce6ec2f0c7e56f37612329d954.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4719ea0ce03035947bc79d6751cad7",
+    "url": "/clothing-shopping-frontend-app/index.html"
+  },
+  {
+    "revision": "d8257f6eb497bf06dcb8",
+    "url": "/clothing-shopping-frontend-app/static/css/main.e97407d0.chunk.css"
+  },
+  {
+    "revision": "5da77f2be6fc26ff7368",
+    "url": "/clothing-shopping-frontend-app/static/js/2.ccb55d20.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/clothing-shopping-frontend-app/static/js/2.ccb55d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8257f6eb497bf06dcb8",
+    "url": "/clothing-shopping-frontend-app/static/js/main.52e9d54d.chunk.js"
+  },
+  {
+    "revision": "490fcaf30007147e3233",
+    "url": "/clothing-shopping-frontend-app/static/js/runtime-main.7618d35f.js"
+  },
+  {
+    "revision": "b1dc952cffb0375bf9ae466479761881",
+    "url": "/clothing-shopping-frontend-app/static/media/blondeafro.b1dc952c.jpg"
+  },
+  {
+    "revision": "dc880f17456d5cad7f07d83554c22b9b",
+    "url": "/clothing-shopping-frontend-app/static/media/chevron-left-solid.dc880f17.svg"
+  },
+  {
+    "revision": "1fce70541cd225507e29c233e23f2cb2",
+    "url": "/clothing-shopping-frontend-app/static/media/chevron-right-solid.1fce7054.svg"
+  },
+  {
+    "revision": "2f9cb5be26eff1a585bf04f1880c914d",
+    "url": "/clothing-shopping-frontend-app/static/media/dogwearingfur.2f9cb5be.jpg"
+  },
+  {
+    "revision": "61c0526999431e76e81ff45dff5c5fa5",
+    "url": "/clothing-shopping-frontend-app/static/media/girlwithskullshirt.61c05269.jpg"
+  },
+  {
+    "revision": "c267a8d986e4cc40fc1e632ea6f44864",
+    "url": "/clothing-shopping-frontend-app/static/media/home__section4--sectionimg--dog.c267a8d9.jpg"
+  },
+  {
+    "revision": "92871b66ca01a9629cea26f880cf2ba4",
+    "url": "/clothing-shopping-frontend-app/static/media/home__section5--icon--jeans.92871b66.jpg"
+  },
+  {
+    "revision": "a169facdc8fce2c692ba3d26656a402f",
+    "url": "/clothing-shopping-frontend-app/static/media/home__section5--icon--skullbrand.a169facd.jpg"
+  },
+  {
+    "revision": "29188f5b6e1b46f404da7fde48de0648",
+    "url": "/clothing-shopping-frontend-app/static/media/home_section3--icon--boys.29188f5b.jpg"
+  },
+  {
+    "revision": "90d13434308c7175c72657cb9fb9e8a1",
+    "url": "/clothing-shopping-frontend-app/static/media/home_section3--icon--girls.90d13434.jpg"
+  },
+  {
+    "revision": "a515b530b3d252defa274c5059c5496a",
+    "url": "/clothing-shopping-frontend-app/static/media/home_section3--icon--women.a515b530.jpg"
+  },
+  {
+    "revision": "68e765a9ee39c701d1dbd61505b0cbec",
+    "url": "/clothing-shopping-frontend-app/static/media/home_section3--icon-mens.68e765a9.jpg"
+  },
+  {
+    "revision": "e8027722f21431849c643e96ffc9485d",
+    "url": "/clothing-shopping-frontend-app/static/media/homeshopaccessories.e8027722.jpg"
+  },
+  {
+    "revision": "0a4883d982ecaf094967653ba261e12a",
+    "url": "/clothing-shopping-frontend-app/static/media/homeshopretro.0a4883d9.jpg"
+  },
+  {
+    "revision": "26ca999744dfef96a9553da1d6e549bc",
+    "url": "/clothing-shopping-frontend-app/static/media/homeshopshoes.26ca9997.jpg"
+  },
+  {
+    "revision": "8bed75b445abe1ea0f8a6150a9aac35f",
+    "url": "/clothing-shopping-frontend-app/static/media/shoeswhitebg.8bed75b4.jpg"
+  }
+]);
